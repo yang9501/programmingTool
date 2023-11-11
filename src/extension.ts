d097dce268c5c8a8ef7e1ed9d197d3c7c3187310
Justin Yang <justinyang@Justins-MacBook-Pro.local>
2023-11-11 01:08:38 -0500

    Added google search feature

--- src/extension.ts
+++ src/extension.ts
@@ -10,14 +10,53 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "helloworld" is now active!');
 
-    context.subscriptions.push(vscode.languages.registerCodeActionsProvider('java', new MyCodeActionProvider()));
+    context.subscriptions.push(vscode.languages.registerCodeActionsProvider('java', new assistedAIFixCodeActionProvider()));
+	context.subscriptions.push(vscode.languages.registerCodeActionsProvider('java', new googleSearchCodeActionProvider()));
 }
 
-export class MyCodeActionProvider implements vscode.CodeActionProvider {
+export class googleSearchCodeActionProvider implements vscode.CodeActionProvider {
+
+	
 	public static readonly providedCodeActionKinds = [
 		vscode.CodeActionKind.QuickFix
 	];
-	//https://github.com/microsoft/vscode-extension-samples/tree/main/code-actions-sample
+
+	public provideCodeActions(document: vscode.TextDocument, range: vscode.Range, context: vscode.CodeActionContext, token: vscode.CancellationToken): vscode.ProviderResult<(vscode.Command | vscode.CodeAction)[]> {
+		let codeActions: vscode.CodeAction[] = [];
+		let editor = vscode.window.activeTextEditor;
+		if (editor) {
+			console.log("hello");
+			let selection = editor.selection.active;
+			let lineNumber = selection.line;
+			console.log("lineNumber = " + lineNumber);
+			let lineOfCode = editor.document.lineAt(lineNumber).text;
+			// The user has right clicked a highlighted syntax error on this line
+			//Ensures that the diagnostics are on the same line as the cursor, therefore only one reference is needed
+			let diagnostics = context.diagnostics.filter(diagnostic => diagnostic.severity === vscode.DiagnosticSeverity.Error && diagnostic.range.start.line === lineNumber);
+			console.log(diagnostics);
+			//Crucial check
+			if (diagnostics.length > 0) {
+				// Display the line of code
+				let codeAction = new vscode.CodeAction('Search for error fix: ' + diagnostics[0].message, vscode.CodeActionKind.QuickFix);
+				
+				let text = encodeURI(lineOfCode);
+				let query = "https://www.google.com/search?q=" + text;
+				codeAction.command = {
+					title: 'Search for error fix',
+					command: 'vscode.open', // The command ID
+					arguments: [vscode.Uri.parse(query)] // The arguments to pass to the command handler
+				};
+				codeActions.push(codeAction);
+			}
+		}
+        return codeActions;
+    }
+}
+
+export class assistedAIFixCodeActionProvider implements vscode.CodeActionProvider {
+	public static readonly providedCodeActionKinds = [
+		vscode.CodeActionKind.QuickFix
+	];
    
 	public provideCodeActions(document: vscode.TextDocument, range: vscode.Range, context: vscode.CodeActionContext, token: vscode.CancellationToken): vscode.ProviderResult<(vscode.Command | vscode.CodeAction)[]> {
 		let codeActions: vscode.CodeAction[] = [];
