4ce9e02e32aed9e03e60939ce001fb064a6c2840
Justin Yang <justinyang@Justins-MacBook-Pro.local>
2023-11-22 00:21:27 -0500

    Improved GPT query results

--- src/extension.ts
+++ src/extension.ts
@@ -13,7 +13,8 @@ export async function activate(context: vscode.ExtensionContext) {
 	vscode.commands.registerCommand('extension.fixSyntaxError', async (lineNumber: number) => {
 		let editor = vscode.window.activeTextEditor;
 		if (editor) {
-			let lineOfCode = editor.document.lineAt(lineNumber).text;
+			let lineOfCode = editor.document.lineAt(lineNumber)
+			let lineOfCodeText = lineOfCode.text;
 			// The user has right clicked a highlighted syntax error on this line
 			//Ensures that the diagnostics are on the same line as the cursor, therefore only one reference is needed
 			let diagnostics = vscode.languages.getDiagnostics(editor.document.uri).filter(diagnostic => diagnostic.severity === vscode.DiagnosticSeverity.Error && diagnostic.range.start.line === lineNumber);
@@ -23,10 +24,12 @@ export async function activate(context: vscode.ExtensionContext) {
 				// Display the line of code
 				vscode.window.showInformationMessage('Fixing syntax error: ' + diagnostics[0].message);
 				// Replace the line of code with the GPT-3 assisted fix
-				let gptAssistedReturnString = await sendErrorToChatGPT(lineOfCode);
+				let gptAssistedReturnString = await sendErrorToChatGPT(lineOfCodeText);
+				let range = lineOfCode.range;
+				let indentation = lineOfCodeText.match(/^\s*/)[0];
 				if (gptAssistedReturnString !== "unmodified") {
 					editor.edit(editBuilder => {
-						editBuilder.replace(diagnostics[0].range, gptAssistedReturnString);
+						editBuilder.replace(range, indentation + gptAssistedReturnString);
 					});
 				}
 			}
--- src/httprequest.ts
+++ src/httprequest.ts
@@ -2,13 +2,17 @@ import OpenAI from "openai";
 
 export async function sendErrorToChatGPT(errorText: string) {
   const openai = new OpenAI({
-    apiKey: "", // TODO: Add API key here
+    apiKey: "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", // TODO: Add API key here
   });
-
+  console.log("errorText = " + errorText);
   const chatCompletion = await openai.chat.completions.create({
-    messages: [{ role: "user", content: "Fix this code:\n" + errorText}],
+    messages: [{ role: "user", content: "Fix this code and return proper code:\n" + errorText}],
     model: "gpt-3.5-turbo",
   });
-
-  return chatCompletion.choices[0].message.toString();
+  console.log(chatCompletion);
+  let gptContent = chatCompletion.choices[0].message.content;
+  if (gptContent == null) {
+    return "unmodified";
+  }
+  return gptContent.toString();
 }
