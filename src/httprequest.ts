8ddf5d252b1bbe117123c0dd578532201c86766e
Justin Yang <justinyang@Justins-MacBook-Pro.local>
2023-11-22 00:24:36 -0500

    plz don't explode

--- src/httprequest.ts
+++ src/httprequest.ts
@@ -2,7 +2,7 @@ import OpenAI from "openai";
 
 export async function sendErrorToChatGPT(errorText: string) {
   const openai = new OpenAI({
-    apiKey: "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", // TODO: Add API key here
+    apiKey: "", // TODO: Add API key here
   });
   console.log("errorText = " + errorText);
   const chatCompletion = await openai.chat.completions.create({
