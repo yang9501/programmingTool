14466f879e472dd23bbe2f42c11c72a7037683a7
Athena Parker <neganote43@gmail.com>
2023-11-21 22:14:18 -0500

    remove secret api key aaaaa

--- src/httprequest.ts
+++ src/httprequest.ts
@@ -2,7 +2,7 @@ import OpenAI from "openai";
 
 export async function sendErrorToChatGPT(errorText: string) {
   const openai = new OpenAI({
-    apiKey: "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", // TODO: Add API key here
+    apiKey: "", // TODO: Add API key here
   });
 
   const chatCompletion = await openai.chat.completions.create({
